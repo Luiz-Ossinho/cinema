84633ac03e833a619ff5faa4f266d2dd3ececf26
luizeduardodjs <luiz.santana059@academico.ifs.edu.br>
2021-03-27 20:31:02 -0300

    Resolvendo conflito

--- src/main/java/prova3bi/Cinema/App.java
+++ src/main/java/prova3bi/Cinema/App.java
@@ -7,16 +7,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-<<<<<<< HEAD
-import java.io.IOException;
-=======
-import prova3bi.Cinema.Data.DBContext;
-import prova3bi.Cinema.Data.Repositories.LoginRepository;
-import prova3bi.Cinema.Domain.Entidades.Login;
-import prova3bi.Cinema.Domain.Entidades.NivelPermissao;
-import prova3bi.Cinema.Domain.Interfaces.Repositories.ILoginRepository;
->>>>>>> refs/remotes/origin/feature/GuilhermeSantosUI
-
 /**
  * JavaFX App
  */
@@ -40,7 +30,6 @@ public class App extends Application {
 		return fxmlLoader.load();
 	}
 
-<<<<<<< HEAD
 	public static void main(String[] args) {
 //		var DH = LocalDateTime.now().plusHours(2);
 //		var DHLater = DH.plusHours(2);
@@ -53,14 +42,4 @@ public class App extends Application {
 // NO MEU FUNCIONOU KKKKKKKK
 		launch();
 	}
-=======
-    public static void main(String[] args) {
-    	ILoginRepository loginRepo = new LoginRepository(new DBContext());
-    	loginRepo.Add(new Login("Usuario1", "SenhaDigitda123", NivelPermissao.Admin));
-    	var login = loginRepo.Get("Usuario1");
-    	System.out.println(Login.Hash("123"));
-        launch();
-    }
->>>>>>> refs/remotes/origin/feature/GuilhermeSantosUI
-
 }
