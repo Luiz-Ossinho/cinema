2f9d7cebc1b31912860e30049eee254192c616a8
marcosl493 <66807489+marcosl493@users.noreply.github.com>
2021-03-27 20:36:02 -0300

    Adicionada a função de gerar pdf com qrcodes

--- src/main/java/prova3bi/Cinema/App.java
+++ src/main/java/prova3bi/Cinema/App.java
@@ -5,7 +5,34 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import prova3bi.Cinema.Domain.Entidades.Ticket;
+
+import java.awt.Color;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Map;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.lowagie.text.Chunk;
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+
+import com.lowagie.text.Image;
+import com.lowagie.text.PageSize;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.Rectangle;
+import com.lowagie.text.pdf.PdfContentByte;
+import com.lowagie.text.pdf.PdfGState;
+import com.lowagie.text.pdf.PdfWriter;
+
+//import api iText
 
 /**
  * JavaFX App
@@ -30,6 +57,8 @@ public class App extends Application {
 		return fxmlLoader.load();
 	}
 
+
+
 	public static void main(String[] args) {
 //		var DH = LocalDateTime.now().plusHours(2);
 //		var DHLater = DH.plusHours(2);
@@ -40,6 +69,12 @@ public class App extends Application {
 //		var list = sessionService.GetNext();
 // CODIGO USADO PARA TESTAR
 // NO MEU FUNCIONOU KKKKKKKK
+		ArrayList<Ticket> tickets = new ArrayList<>();
+		tickets.add(new Ticket(1));
+		tickets.add(new Ticket(2));
+		tickets.add(new Ticket(3));
+		tickets.add(new Ticket(4));
+		Ticket.createPDF(tickets);
 		launch();
 	}
 
--- src/main/java/prova3bi/Cinema/Domain/Entidades/Ticket.java
+++ src/main/java/prova3bi/Cinema/Domain/Entidades/Ticket.java
@@ -1,7 +1,24 @@
 package prova3bi.Cinema.Domain.Entidades;
 
-public class Ticket extends Entidade {
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Image;
+import com.lowagie.text.Rectangle;
+import com.lowagie.text.pdf.PdfWriter;
+
+public class Ticket extends Entidade {
+	private final static String pathQR = "qr.jpg";
+	private final static String pathPDF = "tickets.pdf";
 	public Ticket(int id) {
 		super(id);
 		// TODO Auto-generated constructor stub
@@ -15,5 +32,53 @@ public class Ticket extends Entidade {
 		// TODO Auto-generated method stub
 		return false;
 	}
+	//metodo recebe um 
+	private static Image createQR(int id)
+			throws WriterException, IOException {
+		BitMatrix matrix = new MultiFormatWriter()
+				.encode(Integer.toString(id), BarcodeFormat.QR_CODE, 500, 500);
+		
+		MatrixToImageWriter.writeToPath(matrix, "jpg", Paths.get(pathQR));
+		Image qr = Image.getInstance(pathQR);
+		return qr;
+	}
+	//metodo recebe uma lista de tickets e salva em um pdf os qr codes dos ids de todos os kits
+	public static void createPDF(ArrayList<Ticket> ticket) {
+		Rectangle rect = new Rectangle(500, 550);
+		// step 1: creation of a document-object
+		Document document = new Document(rect, 0,0,0,0);
+		try {
 
+			// step 2:
+			// we create a writer that listens to the document
+			// and directs a PDF-stream to a file
+			PdfWriter.getInstance(document, new FileOutputStream(pathPDF));
+			
+		     
+			Image logo = Image.getInstance("cinenow.png");
+			// step 3: we open the document
+			logo.setAlignment(Image.ALIGN_CENTER);
+			document.open();
+
+			// step 4: we add a paragraph to the document
+			for (int i = 0; i < ticket.size(); i++) {
+				document.add(logo);
+				document.add(Ticket.createQR(ticket.get(i).getId()));
+				if(i > 1) {
+					document.newPage();
+				}
+			}
+			
+		} catch (DocumentException de) {
+			System.err.println(de.getMessage());
+		} catch (IOException ioe) {
+			System.err.println(ioe.getMessage());
+		} catch (WriterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// step 5: we close the document
+		document.close();
+	}
 }
