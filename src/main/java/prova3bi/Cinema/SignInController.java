144da52ff3fbc12d26d90b99f6eeb4b8fd3e51c2
GuilhermeSantosUI <guilherme.contato080@gmail.com>
2021-03-30 19:01:32 -0300

    New features

--- src/main/java/prova3bi/Cinema/SignInController.java
+++ src/main/java/prova3bi/Cinema/SignInController.java
@@ -6,11 +6,12 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.MouseEvent;
-import prova3bi.Cinema.Services.LoginService;
+import prova3bi.Cinema.Domain.Interfaces.Services.ILoginService;
+import prova3bi.Cinema.Services.UnitFactory;
 import prova3bi.Cinema.Util.Alerts;
 
 public class SignInController implements Initializable {
@@ -21,7 +22,7 @@ public class SignInController implements Initializable {
 	@FXML
 	private PasswordField passField;
 
-	private LoginService service;
+	private ILoginService service;
 
 	@FXML
 	private void switchGoBack(MouseEvent event) throws IOException {
@@ -30,22 +31,24 @@ public class SignInController implements Initializable {
 
 	@FXML
 	private void switchLogin() throws IOException {
-		if (!textField.getText().isEmpty() || !passField.getText().isEmpty()) {
+		if (textField.getText().isEmpty() || passField.getText().isEmpty()) {
+			Alerts.showAlert("Ops... aconteceu algum problema!", "Pelo visto você esqueceu de inserir os dados.",
+					"Preencha os campos para continuar!", AlertType.INFORMATION);
+		} else {
+
 			if (service.VerificarUsuario(textField.getText(), passField.getText()) != null) {
 				App.setRoot("Dashboard");
 			} else {
 				Alerts.showAlert("Ops... aconteceu algum problema!",
 						"Aparentemente o usuário ou a senha esta incorreta!", "Tente novamente!", AlertType.ERROR);
 			}
-		} else {
-			Alerts.showAlert("Ops... aconteceu algum problema!", "Pelo visto você esqueceu de inserir os dados.",
-					"Preencha os campos para continuar!", AlertType.INFORMATION);
+
 		}
 	}
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-
+		service = UnitFactory.getLoginService();
 	}
 
 }
