3ec873bfee9c8539a6a70efade8674ab5f1fecaa
luizeduardodjs <luiz.santana059@academico.ifs.edu.br>
2021-04-01 16:08:35 -0300

    Resolvendo conflito

--- src/main/java/prova3bi/Cinema/SignInController.java
+++ src/main/java/prova3bi/Cinema/SignInController.java
@@ -11,11 +11,8 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
-<<<<<<< HEAD
 import prova3bi.Cinema.Domain.Validator;
-=======
 import javafx.scene.layout.Pane;
->>>>>>> refs/remotes/origin/feature/GuilhermeSantosUI
 import prova3bi.Cinema.Domain.Entidades.Login;
 import prova3bi.Cinema.Domain.Entidades.NivelPermissao;
 import prova3bi.Cinema.Domain.Interfaces.Services.ILoginService;
@@ -33,11 +30,6 @@ public class SignInController implements Initializable {
 
 	private ILoginService service;
 
-<<<<<<< HEAD
-=======
-	private Login signedUser;
-	
->>>>>>> refs/remotes/origin/feature/GuilhermeSantosUI
 	@FXML
 	private void switchGoBack(MouseEvent event) throws IOException {
 		App.setRoot("InitialPage");
@@ -45,17 +37,12 @@ public class SignInController implements Initializable {
 
 	@FXML
 	private void switchLogin() throws IOException {
-<<<<<<< HEAD
 		var username = textField.getText();
 		var password = passField.getText();
 
 		if (Validator.isNullOrEmpty(username) || Validator.isNullOrEmpty(password)) {
-=======
-		if (textField.getText().isEmpty() || passField.getText().isEmpty()) {
->>>>>>> refs/remotes/origin/feature/GuilhermeSantosUI
 			Alerts.showAlert("Ops... aconteceu algum problema!", "Pelo visto você esqueceu de inserir os dados.",
 					"Preencha os campos para continuar!", AlertType.INFORMATION);
-<<<<<<< HEAD
 			return;
 		}
 
@@ -86,30 +73,6 @@ public class SignInController implements Initializable {
 			App.setRoot("Dashboard");
 		} catch (IOException e) {
 			e.printStackTrace();
-=======
-		} else {
-			String text1 = textField.getText();
-			String text2 = passField.getText();
-			if (!service.IsFirstLogin()) {
-				Login log = new Login(text1, text2, NivelPermissao.Admin);
-				service.Add(log);
-				this.signedUser = log;
-			} else {
-				Login log = service.VerificarUsuario(text1, text2);
-
-				if (log != null) {
-					this.signedUser = log;
-
-				} else {
-					Alerts.showAlert("Ops... aconteceu algum problema!",
-							"Aparentemente o usuário ou a senha esta incorreta!", "Tente novamente!", AlertType.ERROR);
-				}
-
-			}
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("Dashboard.fxml"));
-			loader.setController(new DashboardController(signedUser));
-			loader.load();
->>>>>>> refs/remotes/origin/feature/GuilhermeSantosUI
 		}
 	}
 	
