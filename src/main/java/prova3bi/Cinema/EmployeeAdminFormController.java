1c956ad59e5a9ee8aee857fdc82bf40ec7a7c330
GuilhermeSantosUI <guilherme.contato080@gmail.com>
2021-04-03 22:46:56 -0300

    add new features

--- src/main/java/prova3bi/Cinema/EmployeeAdminFormController.java
+++ src/main/java/prova3bi/Cinema/EmployeeAdminFormController.java
@@ -1,5 +1,35 @@
 package prova3bi.Cinema;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class EmployeeAdminFormController {
+
+	@FXML
+	private TextField txtUser;
+
+	@FXML
+	private TextField txtPassword;
+
+	@FXML
+	private Label userLabel;
+
+	@FXML
+	private Label passLabel;
+
+	@FXML
+	void onSubmit(ActionEvent event) {
+
+	}
+
+	@FXML
+	void switchGoBack(MouseEvent event) throws IOException {
+		App.setRoot("Dashboard");
+	}
 
 }
