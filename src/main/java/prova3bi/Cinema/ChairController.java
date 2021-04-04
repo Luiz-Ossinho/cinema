2044cb4b343600a4e0fb06ab97010bf96e411dc0
GuilhermeSantosUI <guilherme.contato080@gmail.com>
2021-04-04 19:42:22 -0300

    custom chairs

--- src/main/java/prova3bi/Cinema/ChairController.java
+++ src/main/java/prova3bi/Cinema/ChairController.java
@@ -3,23 +3,34 @@ package prova3bi.Cinema;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import prova3bi.Cinema.Domain.Entities.Chair;
 
-public class ChairController  implements Initializable {
+public class ChairController implements Initializable {
 
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
-		
-	}
-	
+	@FXML
+	private VBox chairContainer;
+
+	@FXML
+	private Label chairPosition;
+
 	private Chair chair;
 
 	public void setChair(Chair chair) {
 		this.chair = chair;
+		chairPosition.setText(chair.column + " " + chair.row);
+
+		if (chair.ocupada) {
+			chairContainer.setStyle("-fx-background-color:  #ff0657; -fx-background-radius: 8;");
+			chairPosition.setStyle("-fx-text-fill: #ffffff;");
+		}
 	}
-	
-	
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+	}
 
 }
