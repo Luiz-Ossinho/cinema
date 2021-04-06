7c4e317c87c87b79993b22e2766c1cc363372282
marcosl493 <marcosvl@protonmail.com>
2021-04-05 22:06:34 -0300

    Remove ignored files e ajustando geracao de pdf

--- src/main/java/prova3bi/Cinema/Domain/Entities/Ticket.java
+++ src/main/java/prova3bi/Cinema/Domain/Entities/Ticket.java
@@ -12,7 +12,9 @@ import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
+import com.lowagie.text.Element;
 import com.lowagie.text.Image;
+import com.lowagie.text.Paragraph;
 import com.lowagie.text.Rectangle;
 import com.lowagie.text.pdf.PdfWriter;
 
@@ -24,8 +26,8 @@ import prova3bi.Cinema.Domain.Validations.ErrorList;
 
 @Table(nome = "Tickets", fks = { "poltrona;Poltronas" })
 public class Ticket extends Entity {
-	private final static String pathQR = "qr.jpg";
-	private final static String pathPDF = "tickets.pdf";
+	private final static String pathQR = ".tempFiles/qr.jpg";
+	private final static String pathPDF = ".tempFiles/tickets.pdf";
 
 	@Builder(Is.Read)
 	public Ticket(int TicketsID, TicketStatus status, int chair) {
@@ -33,7 +35,9 @@ public class Ticket extends Entity {
 		this.status = status;
 		this.poltrona = new Chair(chair);
 	}
-
+	public Ticket(int TicketsID) {
+		super(TicketsID);
+	}
 	@Builder(Is.Insert)
 	public Ticket(Chair chair) {
 		super(-1);
@@ -63,31 +67,37 @@ public class Ticket extends Entity {
 	}
 
 	public static void createPDF(ArrayList<Ticket> ticket) {
-		Rectangle rect = new Rectangle(500, 550);
+		Rectangle rect = new Rectangle(500, 605);
 		Document document = new Document(rect, 0, 0, 0, 0);
 		try {
 
 			PdfWriter.getInstance(document, new FileOutputStream(pathPDF));
 
-			Image logo = Image.getInstance("cinenow.png");
+			Image logo = Image.getInstance("src/main/resources/prova3bi/Images/cinenowLogo.png");
 
 			logo.setAlignment(Image.ALIGN_CENTER);
 			document.open();
-
+			String idTicket;
 			for (int i = 0; i < ticket.size(); i++) {
+				
+				idTicket = Integer.toString(ticket.get(i).getId());
+				Paragraph idCentralizado = new Paragraph(idTicket);
+				idCentralizado.setAlignment(Element.ALIGN_CENTER);
 				document.add(logo);
 				document.add(Ticket.createQR(ticket.get(i).getId()));
-				if (i > 1) {
+				document.add(idCentralizado);
+				if (i > 0) {
 					document.newPage();
 				}
 			}
 
 		} catch (DocumentException de) {
-			System.err.println(de.getMessage());
+			//System.err.println(de.getMessage());
 		} catch (IOException ioe) {
-			System.err.println(ioe.getMessage());
+			//System.err.println(ioe.getMessage());
 		} catch (WriterException e) {
-			e.printStackTrace();
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
 		}
 
 		document.close();
